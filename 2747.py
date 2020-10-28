7793dc09089b73cfbb6324b6ecd60bfa4b185882
Kimhyeonha <heenari0428@naver.com>
2020-10-29 00:23:23 +0900

    baekjoon_2747

---
+++ 2747.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 00:12:20 2020
+
+@author: heena
+"""
+
+n=int(input())
+p=[0,1]
+for i in range(2,n+1):
+    p.append(p[i-1]+p[i-2])
+print(p[n])
