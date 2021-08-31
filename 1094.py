b09d3e31cebb648cc9e866e713907e83f5563ee1
kimhyeonhaa <heenari0428@naver.com>
2021-08-31 13:16:19 +0900

    baekjoon_1094

---
+++ 1094.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 13:04:45 2020
+
+@author: heena
+"""
+
+x=int(input())
+stick=64
+for i in range(x):
+    if stick>x:
+        if (stick/2)>=x:
+            stick=stick/2
+64
+32 32
+16 16
+8 8
